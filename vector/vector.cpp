54532d36a10eda8f706615a09cb81d6fb5e8c718
6Flipped <501536951@qq.com>
2022-07-17 18:16:08 +0800

    vector Analog implementation

---
+++ vector/vector.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+#include"vector.h"
+
+
+int main()
+{
+	//lwz::test1_vector();
+	//lwz::test2_vector();
+	//lwz::test3_vector();
+	//lwz::test4_vector();
+	lwz::test5_vector();
+	return 0;
+}
